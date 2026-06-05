7040ca9ac03a7a84a7136e8aef79d110e3ef68d4
kawa <atletstas@gmail.com>
2026-06-05 19:33:27 +0200

    homework-3.1

--- lesson-1/calculator.py
+++ lesson-1/calculator.py
@@ -1,51 +1,48 @@
-# task_1
-# number = float(input("Enter a number: "))
-# # square = number ** 2
-# # print("The square of", number, "is", square)
+def calculator():
+    print("--- Simple Calculator ---")
 
-# task_2
-# num1 = float(input("Enter first number: "))
-# num2 = float(input("Enter second number: "))
-# num3 = float(input("Enter third number: "))
-#
-# average = (num1 + num2 + num3) / 3
-# print("The average is: ", average)
 
-# task_3
-# total_minutes = int(input("Enter the total minutes: "))
-# hours = total_minutes // 60
-# minutes = total_minutes % 60
-#
-# print(f"{hours}:{minutes}")
+    try:
+        num1 = float(input("Enter the first number: "))
+    except ValueError:
+        print("Error: Please enter a valid number!")
+        return
 
-# task_4
-# price = float(input("Enter your price: "))
-# discount_pecent = float(input("Enter your discount percentage: "))
-#
-# final_price = price * (1 - discount_pecent / 100)
-# print(f"Your final price: {final_price}")
 
-#task 5
-# number = int(input("Enter a number: "))
-# last_digit = number % 10
-# print(f"Last digit: {last_digit}")
+    operation = input("Enter an operation (+, -, *, /): ").strip()
 
-# task 6
-# length = float(input("Enter the length: "))
-# width = float(input("Enter the width: "))
-#
-# perimeter = 2 * (length + width)
-# print(f"Perimeter: {perimeter}")
 
-#task 7
-number = int(input("Enter a 4-digit number: "))
+    try:
+        num2 = float(input("Enter the second number: "))
+    except ValueError:
+        print("Error: Please enter a valid number!")
+        return
 
-digit1 = number // 1000
-digit2 = (number // 100) % 10
-digit3 = (number // 10) % 10
-digit4 = number % 10
 
-print(digit1)
-print(digit2)
-print(digit3)
-print(digit4)
+    if operation == '+':
+        result = num1 + num2
+        print(f"Result: {num1} + {num2} = {result}")
+
+    elif operation == '-':
+        result = num1 - num2
+        print(f"Result: {num1} - {num2} = {result}")
+
+    elif operation == '*':
+        result = num1 * num2
+        print(f"Result: {num1} * {num2} = {result}")
+
+    elif operation == '/':
+
+        if num2 == 0:
+            print("Error: Division by zero is not allowed!")
+        else:
+            result = num1 / num2
+            print(f"Result: {num1} / {num2} = {result}")
+
+    else:
+        print("Error: Invalid operation! Please use +, -, * or /.")
+
+
+
+if __name__ == "__main__":
+    calculator()
